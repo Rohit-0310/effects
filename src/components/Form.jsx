8ac811063e93d937b4d27004ed4d4da6e3dd9293
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-29 16:07:19 +0530

    useEffect Working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Form = () =>{
+
+    const [counter , setCounter] = useState(0);
+    const [age ] = useState(0);
+
     console.log("Before useEffect");
+
     useEffect(() => {
-        console.log("Inside useEffect");
-    });
+        console.log("First Effect")
+    }, [age] )
+
+    useEffect(() => {
+        console.log("Second useEffect");
+    }, [counter] );
+
     console.log("After useEffect");
 
     return (
-        <form>
-            <input type="text" />
-        </form>
+        <div>
+            <h3>Counter: {counter} </h3>
+            <button
+                onClick={() => {
+                    setCounter(counter + 1)
+                }} >Add 1</button>
+
+        </div>
     );
 };
