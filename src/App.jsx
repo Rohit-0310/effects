13c77df6d53da1062c83836430782af70603544f
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-30 14:59:32 +0530

    database Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Form } from './components/Form';
+import { Todo } from './components/Todo';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,49 @@
+import { useEffect,useState } from "react";
+
+export const Todo = () => {
+    const [text, setText] = useState("");
+    const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        getTodo();
+    },[]);
+
+    const getTodo = () => {
+        fetch("http://localhost:3001/todos")
+            .then((d) => d.json())
+            .then((res) => {
+                setTodo(res);
+            });
+    };
+
+
+    const addTodo = () => {
+        const payload = {
+            title : text,
+            status: false,
+        };
+        fetch("http://localhost:3001/todos", {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            ;
+    }
+
+    return (
+        <div>
+            <input 
+                type="text"
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Enter Todo" 
+                />
+                <button onClick={addTodo}>Add Todos</button>
+
+                    {todo.map((e) => (
+                        <div>{e.title}</div>
+                    ))}
+        </div>
+        )
+}
